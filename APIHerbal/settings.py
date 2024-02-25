2252d082034b84c7e5c5bcb4122bd846b15290f3
giabao.oct <80464910+giabao-oct@users.noreply.github.com>
2024-02-25 20:19:30 +0700

    Update settings.py

--- APIHerbal/settings.py
+++ APIHerbal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dz*v@vetti0)oerhjxrcvbdc=8^)d5rkcgf2fs)(zz3*cb&oh1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
